9915db3b288bd18cf0cb350a7d808249ae3cf13e
techocea <dunsfordbright@gmail.com>
2024-07-27 07:11:43 +0530

    Changes

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,11 +1,12 @@
 import { connectToDB } from "@/lib/connectToDB";
 import { NextRequest, NextResponse } from "next/server";
 import Form from "@/models/formModel";
-import nodemailer from "nodemailer";
+
 
 export const POST = async (req: NextRequest) => {
   try {
-    const { fullname, contact, email, country, message, company } = await req.json();
+    const { fullname, contact, email, country, message, company } =
+      await req.json();
 
     await connectToDB();
 
@@ -18,23 +19,6 @@ export const POST = async (req: NextRequest) => {
       company,
     });
     await newForm.save();
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: `New contact form submission from ${fullname}`,
-      text: `Name: ${fullname}\nContact: ${contact}\nCountry: ${country}\nEmail: ${email}\nMessage: ${message}\nCompany: ${company}`,
-    };
-
-    await transporter.sendMail(mailOptions);
 
     return new NextResponse(
       JSON.stringify({ message: "Form submitted successfully", form: newForm }),
