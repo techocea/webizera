fa30ebac4a24b2232673a08f9e5a59d98aa1b245
techocea <dunsfordbright@gmail.com>
2024-07-30 16:15:47 +0530

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="border-t border-gray-500 py-10 text-sm text-center">
             <p className="text-gray-400 ">The best web design agency you`ll ever find</p>
-            <p className="text-gray-400 mt-4"o>Copyright @Webizera - 2024</p>
+            <p className="text-gray-400 mt-4">Copyright @Webizera - 2024</p>
         </footer>
     );
 }
